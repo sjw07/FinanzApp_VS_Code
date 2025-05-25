2d7ecf2a955a07383a06bfeb462c1f7b1592806f
Stefan <117524211+sjw07@users.noreply.github.com>
2025-05-25 21:30:27 +0200

    Remove image asset and add Images folder

---
+++ FinanzApp.WPF/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace FinanzApp.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
